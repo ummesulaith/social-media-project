b8cf218e658a20df195abf7a034405889a002565
Umme Suleyath <ummesul76@gmail.com>
2023-04-15 14:11:43 +0530

    Feature: setup database

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 const http = require('http')
 const app = require('./app')
+const { mongoConnect } = require('./services/mongo')
+
 require('dotenv').config()
 const PORT = process.env.PORT || 8000;
 
 
 const server = http.createServer(app)
 
-function startServer() {
+async function startServer() {
+    await mongoConnect()
     server.listen(PORT, () => {
         console.log('listening to port', PORT)
     })
