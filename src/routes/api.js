b5573fbea58f13894fd34cd781f20f536a1a12cf
Umme Suleyath <ummesul76@gmail.com>
2023-04-23 18:44:12 +0530

    Feature: added post  api

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,12 @@
 const express = require('express'),
 userRouter = require('./user/user.routes'),
-todoRouter = require('./todo/todo.routes');
+todoRouter = require('./todo/todo.routes'),
+postRouter = require('./post/post.routes')
+
 
 api = express.Router()
 api.use('/user', userRouter)
 api.use('/todo', todoRouter)
+api.use('/post', postRouter)
 
 module.exports = api
